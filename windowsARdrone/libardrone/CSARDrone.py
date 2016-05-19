e2ce83656e5034b56af891c95fe521ad094416e2
mjulmer <mulmer@macalester.edu>
2016-05-19 14:53:50 -0500

    CSARdrone updates
    Added a cast to int to deal with a deprecation warning; added a call to
    quit the drone.

--- windowsARdrone/libardrone/CSARDrone.py
+++ windowsARdrone/libardrone/CSARDrone.py
@@ -32,6 +32,7 @@ class PatternFollow(threading.Thread):
                 self.drone.hover()
             with self.lock:
                 runFlag = self.runFlag
+        quit()
 
 
     def patternReact(self, patternInfo):
@@ -102,43 +103,6 @@ class PatternFollow(threading.Thread):
                 print "move_up"
             time.sleep(0.2)
 
-
-    #def patternReact(self, patternInfo):
-        #(x, y), relativeArea, angle = patternInfo
-        #if abs(x - self.cx) > self.width / 9:
-            #if x < self.cx:
-                #self.drone.turn_left()
-                #print "turn_left"
-            #else:
-                #self.drone.turn_right()
-                #print "turn_right"
-            #time.sleep(0.07)
-        #elif abs(angle) > 16:
-            #if angle > 0.0:
-                #self.drone.move_left()
-                #print "move_left"
-            #else:
-                #self.drone.move_right()
-                #print "move_right"
-            #time.sleep(0.3)
-        #elif abs(relativeArea -  self.targetRelativeArea) > 0.015:
-            #if relativeArea < self.targetRelativeArea:
-                #self.drone.move_forward()
-                #print "move_forward"
-            #else:
-                #self.drone.move_backward()
-                #print "move_backward"
-            #time.sleep(0.4)
-        #elif abs(y - self.cy) > self.height / 6:
-            ##height indexes from the top down
-            #if y > self.cy:
-                #self.drone.move_down()
-                #print "move_down"
-            #else:
-                #self.drone.move_up()
-                #print "move_up"
-            #time.sleep(0.2)
-
     def quit(self):
         with self.lock:
             self.runFlag = False
@@ -146,6 +110,7 @@ class PatternFollow(threading.Thread):
         print("Quitting Drone")
         time.sleep(2)
         self.drone.reset()
+        self.drone.halt()
         self.mcs.stop()
         
 

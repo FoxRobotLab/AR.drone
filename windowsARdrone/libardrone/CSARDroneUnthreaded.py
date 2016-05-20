06142c621b13ddf53275a8bfe26d7275293fdf99
rhohle <rhohle@macalester.edu>
2016-05-20 16:24:01 -0500

    Changed windows pattern react.

--- windowsARdrone/libardrone/CSARDroneUnthreaded.py
+++ windowsARdrone/libardrone/CSARDroneUnthreaded.py
@@ -43,108 +43,126 @@ class PatternFollow:  ## removed the thread part of this
 
     def patternReact(self, patternInfo):
         """"""
+        # (x,y) is center point of center color
+        # relativeArea is how big area of two outer colors together is compared to the whole drone image
+        # angle is positive if area of left outer color is smaller than right outer color
         (x, y), relativeArea, angle, _1, _2 = patternInfo
-        
+
+        # Scores made to make hierarchy of "issues" with target in drone's view.
+        # Most problematic (highest score) is the issue the drone tries to solve by moving
         xDiff = abs(x - self.cx)
-        xScore = xDiff / (self.width / 2.0) * (7 / 6.0) # because the edges of the frame are cut off in MCS
-        
+        xScore = xDiff / (self.width / 2.0) * (7 / 6.0)  # because the edges of the frame are cut off in MCS
+
         yDiff = abs(y - self.cy)
         yScore = yDiff / (self.height / 2.0) * (5 / 4.0)
-        
+
         angleScore = abs(angle / 90) * 1.5
-        
+
         areaScore = abs(max((1 - relativeArea / self.targetRelativeArea), -1))
-        
-        print "Angle", angle
-        print "________________________"
-        print "CSARDrone"
-        print "xScore =", xScore
-        print "yScore =", yScore
-        print "angleScore =", angleScore
-        print "areaScore =", areaScore
-        
+
+        print("Angle", angle)
+        print("________________________")
+        print("CSARDrone")
+        print("xScore =", xScore)
+        print("yScore =", yScore)
+        print("angleScore =", angleScore)
+        print("areaScore =", areaScore)
+
         scores = [("xScore", xScore), ("areaScore", areaScore), ("angleScore", angleScore), ("yScore", yScore)]
-        
+
         bestName, bestScore = scores[0]
-        
+
         for score in scores:
             name, num = score
             if num > bestScore:
                 bestName, bestScore = score
-                
+
+        # If none of the scores are big enough to return any issues with the target in the drone's view to avoid
+        # drone constantly trying to fix minute issues
         if bestScore < 0.4:
             return
-        
+
+        # If center color is left/right of drone's view
         if bestName == "xScore":
             if x < self.cx:
-                self.drone.turn_left()
-                print "turn_left"
+                self.drone.move_left()
+                print("move_left")
             else:
-                self.drone.turn_right()
-                print "turn_right"
+                self.drone.move_right()
+                print("move_right")
             time.sleep(0.09)
+
+        # If one outer color has greater area visible to drone than other (meaning color strip is angled)
         elif bestName == "angleScore":
             if angle > 0.0:
                 self.drone.move_left()
-                print "move_left"
+                sleep(0.09)
+                self.drone.turn_right()
+                print("adjust angle right")
             else:
                 self.drone.move_right()
-                print "move_right"
+                sleep(0.09)
+                self.drone.turn_left()
+                print("adjust angle left")
             time.sleep(0.43)
+
+        # If target area does not take up enough area of drone's view (too far away/close-up)
         elif bestName == "areaScore":
             if relativeArea < self.targetRelativeArea:
                 self.drone.move_forward()
-                print "move_forward"
+                print("move_forward")
             else:
                 self.drone.move_backward()
-                print "move_backward"
+            print("move_backward")
             time.sleep(0.45)
+
+        # If center color is too high/low in drone's view
         elif bestName == "yScore":
-            #height indexes from the top down
+            # height indexes from the top down
             if y > self.cy:
                 self.drone.move_down()
-                print "move_down"
+                print("move_down")
             else:
                 self.drone.move_up()
-                print "move_up"
+                print("move_up")
             time.sleep(0.2)
 
+            # if bestName == "xScore":
+            #     if x < self.cx:
+            #         self.drone.move_left()
+            #         print("move_left")
+            #     else:
+            #         self.drone.move_right()
+            #         print("move_right")
+            #     time.sleep(0.09)
+            # elif bestName == "angleScore":
+            #     if angle > 0.0:
+            #         self.drone.move_left()
+            #         self.drone.turn_right()
+            #         print("adjust angle right")
+            #     else:
+            #         self.drone.move_right()
+            #         self.drone.turn_left()
+            #         print("adjust angle left")
+            #     time.sleep(0.20)
+            # elif bestName == "areaScore":
+            #     if relativeArea < self.targetRelativeArea:
+            #         self.drone.move_forward()
+            #         print("move_forward")
+            #     else:
+            #         self.drone.move_backward()
+            #     print("move_backward")
+            #     time.sleep(0.45)
+            # elif bestName == "yScore":
+            #     # height indexes from the top down
+            #     if y > self.cy:
+            #         self.drone.move_down()
+            #         print("move_down")
+            #     else:
+            #         self.drone.move_up()
+            #         print("move_up")
+            #     time.sleep(0.2)
 
-    #def patternReact(self, patternInfo):
-        #(x, y), relativeArea, angle = patternInfo
-        #if abs(x - self.cx) > self.width / 9:
-            #if x < self.cx:
-                #self.drone.turn_left()
-                #print "turn_left"
-            #else:
-                #self.drone.turn_right()
-                #print "turn_right"
-            #time.sleep(0.07)
-        #elif abs(angle) > 16:
-            #if angle > 0.0:
-                #self.drone.move_left()
-                #print "move_left"
-            #else:
-                #self.drone.move_right()
-                #print "move_right"
-            #time.sleep(0.3)
-        #elif abs(relativeArea -  self.targetRelativeArea) > 0.015:
-            #if relativeArea < self.targetRelativeArea:
-                #self.drone.move_forward()
-                #print "move_forward"
-            #else:
-                #self.drone.move_backward()
-                #print "move_backward"
-            #time.sleep(0.4)
-        #elif abs(y - self.cy) > self.height / 6:
-            ##height indexes from the top down
-            #if y > self.cy:
-                #self.drone.move_down()
-                #print "move_down"
-            #else:
-                #self.drone.move_up()
-                #print "move_up"
-            #time.sleep(0.2)
 
     def quit(self):
         with self.lock:
